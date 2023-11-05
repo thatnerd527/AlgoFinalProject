c7e59a5949f240fee83199bba1007f3ce1ab16a7
Muhammad Fadhlan <muhammadfadhlan4@gmail.com>
2023-11-05 17:08:38 +0700

    first component

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,40 @@
+import java.util.ArrayList;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Table testtable = new Table();
+    testtable.columnames.add("Column 1");
+    testtable.columnames.add("Column 2");
+    testtable.columnames.add("Column 3dddddddddddddddddddddddddddddddddddddddddddd");
+    {
+      // Row 1
+      ArrayList<String> row1 = new ArrayList<String>();
+      row1.add("11111 1336673462734273 222192369242137");
+      row1.add("22222231d");
+      row1.add("2222223123132132d1");
+      testtable.rowsandcolumns.add(row1);
+    }
+
+    {
+      // Row 2
+      ArrayList<String> row1 = new ArrayList<String>();
+      row1.add("11111 1336673462734273 222192");
+      row1.add("2222223123132132d1");
+      row1.add("2222223123132132d1");
+      testtable.rowsandcolumns.add(row1);
+    }
+
+    {
+      // Row 3
+      ArrayList<String> row1 = new ArrayList<String>();
+      row1.add("11111 133667346");
+      row1.add("222d1 12313 21");
+      row1.add("2222223123132132d1");
+      testtable.rowsandcolumns.add(row1);
+    }
+
+    System.out.print(Table.getPrintedTable(testtable,true));
+
 
   }
 }
--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -1,3 +1,108 @@
-public class Table {
+import java.util.ArrayList;
 
+public class Table {
+    public ArrayList<String> columnames = new ArrayList<String>();
+    public ArrayList<ArrayList<String>> rowsandcolumns = new ArrayList<ArrayList<String>>();
+
+    public void addRow(ArrayList<String> row) {
+        rowsandcolumns.add(row);
+    }
+
+    public static String getPrintedTable(Table table, boolean withheader) {
+        ArrayList<ArrayList<String>> tempbuilder = new ArrayList<ArrayList<String>>();
+        if (withheader) {
+            tempbuilder.add(table.columnames);
+        }
+        tempbuilder.addAll(table.rowsandcolumns);
+
+        // First we calculate the column widths
+        ArrayList<Integer> columnwidth = new ArrayList<Integer>();
+        // for all rows
+        for (ArrayList<String> column : tempbuilder) {
+            for (int i = 0; i < column.size(); i++) {
+                try {
+                    columnwidth.get(i);
+                    columnwidth.set(i, Math.max(columnwidth.get(i), column.get(i).length()));
+                } catch (Exception e) {
+                    columnwidth.add(column.get(i).length());
+                }
+
+            }
+        }
+
+        StringBuilder builtable = new StringBuilder();
+
+        // First the header of the table.
+        /// +--------+-------------+
+        builtable.append("+");
+        for (Integer integer : columnwidth) {
+            builtable.append(stringGenerator("-", integer));
+            builtable.append("+");
+        }
+        builtable.append("\n");
+
+        if (withheader) {
+            // Second, the columnnames.
+            /// |dkadjsdj|ddddddd331331|
+            builtable.append("|");
+            for (int i = 0; i < table.columnames.size(); i++) {
+                builtable.append(stringOverlayer(stringGenerator(" ", columnwidth.get(i)), table.columnames.get(i)));
+                builtable.append("|");
+            }
+            builtable.append("\n");
+
+            // Third, another header
+            /// +--------+-------------+
+            builtable.append("+");
+            for (Integer integer : columnwidth) {
+                builtable.append(stringGenerator("-", integer));
+                builtable.append("+");
+            }
+            builtable.append("\n");
+        }
+        // Fourth, now its time to render the items.
+        // for all rows
+        for (ArrayList<String> columns : table.rowsandcolumns) {
+            builtable.append("|");
+            for (int i = 0; i < columns.size(); i++) {
+                builtable.append(stringOverlayer(stringGenerator(" ", columnwidth.get(i)), columns.get(i)));
+                builtable.append("|");
+            }
+            builtable.append("\n");
+        }
+
+        // Fifth, now rendering the footer.
+        /// +--------+-------------+
+        builtable.append("+");
+        for (Integer integer : columnwidth) {
+            builtable.append(stringGenerator("-", integer));
+            builtable.append("+");
+        }
+        builtable.append("\n");
+
+        return builtable.toString();
+
+    }
+
+    public static String stringOverlayer(String base, String overlay) {
+        char[] work = base.toCharArray();
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        for (char character : overlay.toCharArray()) {
+            work[i] = character;
+            i++;
+        }
+        for (char c : work) {
+            result.append(c);
+        }
+        return result.toString();
+    }
+
+    public static String stringGenerator(String txt, Integer amount) {
+        String result = "";
+        for (int i = 0; i < amount; i++) {
+            result += txt;
+        }
+        return result;
+    }
 }
