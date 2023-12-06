8c1a59f3e31a40abef8326be3480a86fa73bce60
Muhammad Fadhlan <muhammadfadhlan4@gmail.com>
2023-12-06 19:39:34 +0700

    dasd das d d ad

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -9,13 +9,14 @@ import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 import Material.MaterialDatabase;
 
 public class Server {
     public static MaterialDatabase currentlystored = new MaterialDatabase();
     public static MaterialDatabase templatematerials = new MaterialDatabase();
-
+    public static Logger logger = Logger.getLogger(Server.class.getName());
     static byte[] readAsBytes(File file) throws IOException {
         int length = (int) file.length();
         char[] output = new char[length];
@@ -41,21 +42,22 @@ public class Server {
                     Scanner a = new Scanner(client.getInputStream());
                     try {
                         session = a.nextLine();
-                        System.out.println("Session id: " + session);
+
+                        logger.info("Session " + session + " connected.");
                         SessionManager.joinByString(session,new InputStreamReader(client.getInputStream()), new OutputStreamWriter(client.getOutputStream()));
                     } catch (IOException e) {
-                        System.out.println("Could not get session id.");
+                        logger.warning("An anonymouse session's id was unable to be fetched.");
                     }
 
                 }
             } catch (IOException e) {
-                System.out.println("Server closed.");
+                logger.warning("Server closed.");
             }
         }).start();
     }
 
     public static void StaticInit() {
-        System.out.println("Starting as server.");
+        logger.info("Starting as server.");
         try {
             currentlystored = MaterialDatabase.load(readAsBytes(new File(("./current.bin"))));
             templatematerials = MaterialDatabase.load(readAsBytes(new File("./template.bin")));
@@ -69,6 +71,8 @@ public class Server {
                 saveToWriter(new FileWriter("./template.bin"), templatematerials.save());
                 StartServer();
             } catch (IOException e2) {
+                logger.severe("Cant save initial files, exiting immedeatly");
+                System.exit(1);
             }
         }
     }
